e639b00a0d1456041b35a850439ae0dc4975a3ff
Jan Urbansky <jan.urbansky91@gmail.com>
2018-03-24 16:42:51 +0100

    Add comments and format stuff

--- client/client.go
+++ client/client.go
@@ -1,31 +1,35 @@
 package client
 
 import (
+	"encoding/json"
 	"errors"
 	"log"
 
 	god "github.com/JanUrb/godathon2018"
+	"github.com/JanUrb/godathon2018/protocol"
 	"github.com/gorilla/websocket"
 )
 
 var (
-	ErrWrite error = errors.New("ErrWrite")
+	//ErrWrite indicates that an error while writing happened.
+	ErrWrite = errors.New("ErrWrite")
 )
 
+//Client represents a websocket client.
 type Client struct {
-	protocol god.Protocol
+	clientID int
+	groupID  int
 	switcher god.Switching
 	conn     *websocket.Conn //gorilla/websocket uses pointer types for connection
 }
 
-var _ god.Client = &Client{} //compile time interface check
+var _ god.Client = (*Client)(nil) //compile time interface check
 
 //New returns a new instance of the Client struct.
-func New(protocol god.Protocol, switcher god.Switching, conn *websocket.Conn) *Client {
+func New(switcher god.Switching, conn *websocket.Conn) *Client {
 	return &Client{
-		protocol,
-		switcher,
-		conn,
+		switcher: switcher,
+		conn:     conn,
 	}
 }
 
@@ -35,12 +39,26 @@ func (c *Client) Listen() {
 		websocketMessageType, b, err := c.conn.ReadMessage()
 		if err != nil {
 			log.Println("Error while reading message from connection. ", err)
-			//TODO switching remove client
+			err := c.switcher.DetachGroup(c.groupID, c.clientID)
+			if err != nil {
+				log.Println("Error while detaching from group: ", c.groupID, " with client: ", c.clientID)
+				return
+			}
 			return
 		}
 
 		log.Println("Message type: ", websocketMessageType)
 		//TODO protocol decode message
+
+		var genericMsg protocol.Generic_message
+		err = json.Unmarshal(b, &genericMsg)
+		if err != nil {
+			log.Printf("Error while reading generic message. (Client Id: %d, message: %s)", c.clientID, genericMsg)
+			continue // continue reading messages. No need to kill the client
+		}
+
+
+
 		log.Println("Message content: ", b)
 
 	}
@@ -54,4 +72,45 @@ func (c *Client) Write(data []byte) error {
 		return ErrWrite
 	}
 	return nil
+}
+
+
+
+func (c *Client) resolveMessage(messageType int){
+	switch(messageType){
+	case: 
+	}
+}
+
+
+
+//OnTxCeasedAck sends to the underlying connection
+func (c *Client) OnTxCeasedAck() {
+
+	panic("not implemented")
+}
+
+//OnTxInfoInd sends to the underlying connection
+func (c *Client) OnTxInfoInd() {
+	panic("not implemented")
+}
+
+//OnTxDemandAck sends to the underlying connection
+func (c *Client) OnTxDemandAck() {
+	panic("not implemented")
+}
+
+//OnSetupAck sends to the underlying connection
+func (c *Client) OnSetupAck() {
+	panic("not implemented")
+}
+
+//OnSetupInd sends to the underlying connection
+func (c *Client) OnSetupInd() {
+	panic("not implemented")
+}
+
+//OnConnectAck sends to the underlying connection
+func (c *Client) OnConnectAck() {
+	panic("not implemented")
 }
