dbc320649ce0fce15c902b03d824caa81d08948d
Jan Urbansky <jan.urbansky91@gmail.com>
2018-03-24 15:38:57 +0100

    Fixed signature of AttachClientToGroup

--- domain.go
+++ domain.go
@@ -1,7 +1,5 @@
 package godathon2018
 
-import "github.com/JanUrb/godathon2018"
-
 const example = 1
 
 type Client interface {
@@ -17,7 +15,7 @@ type Web interface{}
 
 type Switching interface {
 	Call([]byte, int)
-	AttachClientToGroup(int, int, godathon2018.Client) error
+	AttachClientToGroup(int, int, Client) error
 	DetachClientFromGroup(int, int) error
 	SetCaller(int, int) error
 	RemoveCaller(int, int) error
