ae9039277814c95480ca3a3200fae454b3e3e59b
Jan Urbansky <jan.urbansky91@gmail.com>
2018-03-24 13:22:11 +0100

    cleaned up interfaces

--- domain.go
+++ domain.go
@@ -2,10 +2,14 @@ package godathon2018
 
 const example = 1
 
-type client interface {
+type Client interface {
 	write([]byte) error
 }
 
-type protocol interface {
+type Protocol interface {
 	decode([]byte) (error, int, interface{})
 }
+
+type Web interface{}
+
+type Auth interface{}
