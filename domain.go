d74367ef91f158f333610913650aeeac6cc69c3c
Jan Urbansky <jan.urbansky91@gmail.com>
2018-03-24 13:25:42 +0100

    Uppercase is public

--- domain.go
+++ domain.go
@@ -3,11 +3,11 @@ package godathon2018
 const example = 1
 
 type Client interface {
-	write([]byte) error
+	Write([]byte) error
 }
 
 type Protocol interface {
-	decode([]byte) (error, int, interface{})
+	Decode([]byte) (error, int, interface{})
 }
 
 type Web interface{}
