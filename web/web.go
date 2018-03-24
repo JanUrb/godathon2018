f89297490891c3ff770d6a6bd5f348e2c1b85146
fufanu <philip@philiphermes.de>
2018-03-24 15:13:34 +0100

    Create simple webserver to provide http context and register new clients

---
+++ web/web.go
@@ -0,0 +1,42 @@
+package web
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+
+	god "github.com/JanUrb/godathon2018"
+	"github.com/JanUrb/godathon2018/client"
+)
+
+// Web - Struct for web specific things?
+type Web struct {
+	god.Protocol
+	god.Switching
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    2048,
+	WriteBufferSize:   2048,
+	EnableCompression: false,
+}
+
+// Run Websocket server to allow client registrations
+func (web Web) Run() {
+	log.Println("Starting webserver")
+	http.HandleFunc("/", web.registerClient)
+	log.Println("Waiting for connections")
+	log.Fatal(http.ListenAndServe(":4242", nil))
+}
+
+func (web Web) registerClient(resWriter http.ResponseWriter, req *http.Request) {
+	conn, err := upgrader.Upgrade(resWriter, req, nil)
+	if err != nil {
+		log.Println("Error while establishing websocket connection")
+		return
+	}
+	// Spawn new client
+	client.New(web.Protocol, web.Switching, conn)
+	log.Printf("New client request [IP:%s]\t", conn.RemoteAddr().String())
+}
